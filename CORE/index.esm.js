c5e0f769684c4575fc67b895b52c351df413593a
Tushar Halder <tsrhd01@gmail.com>
2025-10-09 08:06:12 +0530

    Add files via upload

---
+++ CORE/index.esm.js
@@ -0,0 +1,13 @@
+// index.js
+var observer = new MutationObserver(() => {
+  const elm = document.getElementById("daily_note_svg");
+  if (elm) {
+    runFunction();
+    observer.disconnect();
+  }
+});
+observer.observe(document.body, { childList: true, subtree: true });
+function runFunction() {
+  console.log("Running after element loaded");
+  window.myApp.sayHello();
+}
