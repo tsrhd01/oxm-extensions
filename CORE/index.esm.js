d1603c889bda9a9a25331a870ee79f0a588b5b53
Tushar Halder <tsrhd01@gmail.com>
2025-10-10 07:35:14 +0530

    Add files via upload

---
+++ CORE/index.esm.js
@@ -0,0 +1,14 @@
+// btnClick.js
+var dom = document.createElement("div");
+dom.innerHTML = `
+  <div class="count_container" style="position: fixed, top:0, left:0, z-index: 99999;">
+    <div class="total_count" id="the_x_btn">Hello World</div>
+  </div>
+`;
+function bust() {
+  console.log("hello");
+}
+document.querySelector("#the_x_btn").addEventListener("click", () => {
+  bust();
+});
+document.body.appendChild(dom);
